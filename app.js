2f525bf1fa9065dbc8df44c696101994d5d64857
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-12-01 14:53:46 +0100

    First setup of App

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Example app listening at http://%s:%s', host, port)
+
+})
+
+var Twit = require('twit')
+
+var T = new Twit({
+    consumer_key:         'hcYtx0R3HwkCGuFCMCqu43V0G'
+    , consumer_secret:      'PEEt6OVFsOI8T0g2ZWnOu9UUX8bJgVS7mFswxaY3vXGfUzPfYZ'
+    , access_token:         '168580860-uGyScXihhNEvSEWm3MOwpXlrZaPPLV5CbScBLyyc'
+    , access_token_secret:  'mxmWeJX8MWHjLEzu991mKgZv9iaCQlLb4J4bAIX7FAsFX'
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+})
+
+app.get('/search', function (req, res) {
+    T.get('search/tweets', { q: 'competait', count: 1 }, function(err, data, response) {
+        //res.send('Hello World!');
+        res.send(data);
+    });
+
+})
